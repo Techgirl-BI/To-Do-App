606d73c7254667cd58c981a60770f9803bbcff83
Techgirl-FE <rachealonimisi31@gmail.com>
2024-01-11 16:51:23 -0100

    Added package json, index.js, .env and .gitignore files

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import mongoose from "mongoose"
+
+const app = express()
+//parse data into json format
+app.use(express.json())
+
+const {ENV,PORT} = process.env
+//port
+const port = ENV === "development"? PORT : 6000
+app.listen(()=> console.log( `App is listening on ${port}`))
